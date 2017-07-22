d89134ad8058219105572d6d4002370f89509226
Alex <sp115-29@stu.zsea.edu.ua>
2017-07-22 17:01:37 +0300

    New travis
    Travis update 1.

---
+++ ci/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ev
+
+echo "Compilation of JWP Plugins"
+for file in addons/sourcemod/scripting/jwp_*.sp
+do
+  addons/sourcemod/scripting/spcomp -E -v0 $file
+done
+
+# TODO Add afterbuild 
