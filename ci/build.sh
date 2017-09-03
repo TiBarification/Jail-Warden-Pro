43c112bf3325b902ca913fc1db05a6a70ba72661
TiBarification <tishinab2@gmail.com>
2017-09-03 21:31:44 +0300

    Force build #2

--- ci/build.sh
+++ ci/build.sh
@@ -5,5 +5,9 @@ echo "Compilation of JWP Plugins"
 for file in addons/sourcemod/scripting/jwp_*.sp
 do
   echo "Compile $file"
-  addons/sourcemod/scripting/spcomp -E -v0 -o"addons/sourcemod/plugins/$file.smx" $file
+  addons/sourcemod/scripting/spcomp -E -v0 $file
 done
+
+echo ""
+echo "Moving compiled plugins to `plugins` directory"
+mv addons/sourcemod/scripting/jwp_*.smx addons/sourcemod/plugins
