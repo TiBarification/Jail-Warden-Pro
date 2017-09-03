bbb0bebb4bb83069e7f8046f396cd5648ec38350
TiBarification <tishinab2@gmail.com>
2017-09-03 21:19:00 +0300

    Travis fix
    Moved compiled plugins to `plugins` folder.

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ev
+
 echo "Compilation of JWP Plugins"
 for file in addons/sourcemod/scripting/jwp_*.sp
 do
-  addons/sourcemod/scripting/spcomp -E -v0 $file
-  echo ""
+  echo "Compile $file"
+  addons/sourcemod/scripting/spcomp -E -v0 -o"$pwd/addons/sourcemod/plugins/$file.smx" $file
 done
--- ci/test.sh
+++ ci/test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 set -ev
 
 echo "Compilation of JWP Plugins"
 for file in addons/sourcemod/scripting/jwp_*.sp
 do
+  echo "Compile $file"
   addons/sourcemod/scripting/spcomp -E -v0 $file
-  echo ""
 done
