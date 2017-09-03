0d1c22f0125a0e0f75b639bdbd5d2e54a19813ec
TiBarification <tishinab2@gmail.com>
2017-09-03 21:22:44 +0300

    Force build

--- ci/build.sh
+++ ci/build.sh
@@ -5,5 +5,5 @@ echo "Compilation of JWP Plugins"
 for file in addons/sourcemod/scripting/jwp_*.sp
 do
   echo "Compile $file"
-  addons/sourcemod/scripting/spcomp -E -v0 -o"$pwd/addons/sourcemod/plugins/$file.smx" $file
+  addons/sourcemod/scripting/spcomp -E -v0 -o"addons/sourcemod/plugins/$file.smx" $file
 done
